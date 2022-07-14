62507696f1d36728b156463b5d401a554e7f6df6
jcryptodev <jdl.lourens@gmail.com>
2022-07-14 16:04:18 +0200

    initial push
    added resource files

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// showPage function
+//This function will create and insert/append the elements needed to display a "page" of nine students
+
+function showPage(list, page) {}
+
+/*
+Create the `addPagination` function
+This function will create and insert/append the elements needed for the pagination buttons
+*/
+
+// Call functions
