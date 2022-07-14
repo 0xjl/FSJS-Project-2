499d4929335fa2726a079a56beb94ec953e56d59
jcryptodev <jdl.lourens@gmail.com>
2022-07-14 16:06:46 +0200

    added showPage function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 // showPage function
 //This function will create and insert/append the elements needed to display a "page" of nine students
 
-function showPage(list, page) {}
+function showPage(list, page) {
+  let pageNumber;
+  const linkList = document.querySelector(".link-list");
+  linkList.innerHTML = "";
+}
 
 /*
 Create the `addPagination` function
